133ca226482346088540779a6dbebf415cac5af4
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-04-16 21:59:29 +0700

    update neovim

--- .config/nvim/lua/plugins/configs/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/configs/nvim-lspconfig.lua
@@ -56,89 +56,47 @@ for _, lsp_server in ipairs(lsp_servers) do
   })
 end
 
-
--- keymap for lsp if not use lspsaga
--- vim.api.nvim_create_autocmd('LspAttach', {
---   desc = 'LSP actions',
---   callback = function()
---     local bufmap = function(mode, lhs, rhs)
---       local opts = { buffer = true }
---       vim.keymap.set(mode, lhs, rhs, opts)
---     end
-
---     -- Displays hover information about the symbol under the cursor
---     bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-
---     -- Jump to the definition
---     bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-
---     -- Jump to declaration
---     bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-
---     -- Lists all the implementations for the symbol under the cursor
---     bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-
---     -- Jumps to the definition of the type symbol
---     bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-
---     -- Lists all the references
---     bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-
---     -- Displays a function's signature information
---     bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-
---     -- Renames all references to the symbol under the cursor
---     bufmap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<cr>')
-
---     -- Selects a code action available at the current cursor position
---     bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
---     bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
-
---     -- Show diagnostics in a floating window
---     bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-
---     -- Move to the previous diagnostic
---     bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-
---     -- Move to the next diagnostic
---     bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
---   end
--- })
-
-local sign = function(opts)
-  vim.fn.sign_define(opts.name, {
-    texthl = opts.name,
-    text = opts.text,
-    numhl = ''
+local function lspSymbol(name, icon)
+  local hl = "DiagnosticSign" .. name
+  vim.fn.sign_define(hl, {
+    text = icon, numhl = hl, texthl = hl
   })
 end
 
-sign({ name = 'DiagnosticSignError', text = '' })
-sign({ name = 'DiagnosticSignWarn', text = '' })
-sign({ name = 'DiagnosticSignHint', text = '' })
-sign({ name = 'DiagnosticSignInfo', text = '' })
+lspSymbol("Error", "")
+lspSymbol("Info", "")
+lspSymbol("Hint", "")
+lspSymbol("Warn", "")
 
-vim.diagnostic.config({
-  virtual_text = false,
-  severity_sort = true,
-  float = {
-    border = 'single',
-    source = 'always',
-    focusable = true,
-    style = "minimal",
-    header = "",
-    prefix = "",
-    width = 65,
+vim.diagnostic.config {
+  virtual_text = {
+    prefix = "",
   },
-})
+  signs = true,
+  underline = true,
+  severity_sort = true,
+  update_in_insert = false,
+  -- float = {
+  --   border = 'single',
+  --   source = 'always',
+  --   focusable = true,
+  --   style = "minimal",
+  --   header = "",
+  --   prefix = "",
+  --   width = 65,
+  -- },
+}
 
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {
+    border = "single",
+    focusable = false,
+    relative = "cursor",
+  }
+)
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
   vim.lsp.handlers.hover,
-  { border = 'single' }
-)
-
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-  vim.lsp.handlers.signature_help,
   { border = 'single' }
 )
 
