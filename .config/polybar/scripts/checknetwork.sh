647a67d90398e7d6932c76ab4d6114d519a3096d
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-09-19 07:03:22 +0700

    perl: improve check_network script for polybar config

--- .config/polybar/scripts/checknetwork.sh
+++ .config/polybar/scripts/checknetwork.sh
@@ -5,12 +5,8 @@ WIRELESS_CONNECTED_ICON=" "
 ETHERNET_CONNECTED_ICON=" "
 
 # vnpt || viettel || fpt ||google || archlinux
-if (ping -c 1 -q -w 1 203.162.4.191 >/dev/null ||
-	ping -c 1 -q -w 1 203.113.131.1 >/dev/null ||
-	ping -c 1 -q -w 1 210.245.0.10 >/dev/null ||
-	ping -c 1 -q -w 2 google.com >/dev/null ||
-	ping -c 1 -q -w 2 archlinux.org >/dev/null); then
-	if (ip route get 8.8.8.8 | grep -Po 'dev \K\w+' | grep -qFf - /proc/net/wireless >/dev/null); then
+if (ping -c 1 -q -w 1 203.162.4.191 >/dev/null 2>&1 || ping -c 1 -q -w 1 203.113.131.1 >/dev/null 2>&1 || ping -c 1 -q -w 1 210.245.0.10 >/dev/null 2>&1 || ping -c 1 -q -w 2 google.com >/dev/null 2>&1 || ping -c 1 -q -w 2 archlinux.org >/dev/null 2>&1); then
+	if (ip route get 8.8.8.8 | grep -Po 'dev \K\w+' | grep -qFf - /proc/net/wireless >/dev/null 2>&1); then
 		WIFI_NAME=$(nmcli -t -f active,ssid dev wifi | awk -F ':' '$1=="yes" {print $2}')
 		echo "$WIRELESS_CONNECTED_ICON $WIFI_NAME"
 		exit 0
